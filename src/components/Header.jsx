47447eac05d06fe835a19b10fa004ac8549b9346
Leo Freitas <leofreeitas.lfm@gmail.com>
2025-05-22 02:27:26 -0300

    fix title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <header className="py-4 text-center shadow mb-4">
-      <h1 className="fs-2 fw-bold mb-4">Catálago de Filmes</h1>
+      <h1 className="fs-2 fw-bold mb-4">Catálogo de Filmes</h1>
       <nav className="d-flex justify-content-center gap-4 fs-6 fw-semibold">
         <Link to="/" className="text-dark text-decoration-none px-2">Início</Link>
         <Link to="/criar" className="text-dark text-decoration-none px-2">Criar</Link>
